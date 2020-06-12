9802651c9d63fb54aa9fddd5522b728dcf31a4c0
aait <info@aait.se>
2020-06-12 17:45:41 +0600

    Make payment_token_class property optional

--- src/SwedbankPay/Core/Adapter/WC_Adapter.php
+++ src/SwedbankPay/Core/Adapter/WC_Adapter.php
@@ -546,17 +546,18 @@ class WC_Adapter extends PaymentAdapter implements PaymentAdapterInterface
         $expiry_date,
         $order_id = null
     ) {
-        $expiry_date = explode('/', $expiry_date);
+        if (!property_exists($this->gateway, 'payment_token_class')) {
+            return;
+        }
 
         // Create Payment Token
-        if (!property_exists($this->gateway, 'payment_token_class') ||
-            empty($this->gateway->payment_token_class)
-        ) {
+        if (!is_string($this->gateway->payment_token_class) || !class_exists($this->gateway->payment_token_class, false)) {
             throw new \Exception(__('Payment Token class is undefined.', 'swedbank-pay-woocommerce-payments'));
         }
 
-        $token = new $this->gateway->payment_token_class;
+        $expiry_date = explode('/', $expiry_date);
 
+        $token = new $this->gateway->payment_token_class;
         $token->set_gateway_id($this->gateway->id);
         $token->set_token($payment_token);
         $token->set_recurrence_token($recurrence_token);
